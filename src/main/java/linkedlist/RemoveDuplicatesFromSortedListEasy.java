04fb59a3a4b066ce924c725b6df22a3d7ceaf2f8
Yam Ferreira <yamferreira6@gmail.com>
2026-07-11 00:29:03 -0300

    feat: add annotations

--- src/main/java/linkedlist/RemoveDuplicatesFromSortedListEasy.java
+++ src/main/java/linkedlist/RemoveDuplicatesFromSortedListEasy.java
@@ -16,16 +16,16 @@ package linkedlist;
 public class RemoveDuplicatesFromSortedListEasy {
     public ListNode deleteDuplicate(ListNode head) {
 
-        ListNode res = head;
+        ListNode res = head;                                // guarda a referencia do inicio para retornar no final
 
-        while (head != null && head.next != null) {
-            if (head.val == head.next.val) {
-                head.next = head.next.next;
-            } else {
-                head = head.next;
+        while (head != null && head.next != null) {         // enquanto o atual e O PROXIMO existirem continua lupando
+            if (head.val == head.next.val) {                // se o valor do head atual for igual ao valor do proximo head
+                head.next = head.next.next;                 // o proximo head pula para a outra casa
+            } else {                                        // se o head atual nao for igual ao proximo head
+                head = head.next;                           // o head que avanca uma casa
             }
         }
 
-        return res;
+        return res;                                         //aqui retorna a lista sem os iguais
     }
 }
